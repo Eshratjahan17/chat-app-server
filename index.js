00f6dda70ad69279c47d32105de5cdc23d9b2ff6
Eshratjahan17 <eshratjahan701@gmail.com>
2022-09-22 14:11:09 +0600

    user post api

--- index.js
+++ index.js
@@ -8,11 +8,33 @@ app.use(express.json());
 const port =process.env.PORT||5000;
 
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser:true, useUnifiedTopology:true  }, (err) => {
- 
-    console.log("db connected");
-  
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.iwsmtfs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
+async function run(){
+  try{
+
+     await client.connect();
+     const usersCollection = client.db("chat-app").collection("users");
+
+      app.post("/user", async (req, res) => {
+        const data = req.body;
+        const result = await usersCollection.insertOne(data);
+        res.send(result);
+      });
+
+  }finally{
+
+  }
+  console.log("db connected");
+}
+run().catch(console.dir);
+
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
